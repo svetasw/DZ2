2264e4ddda61377adcd67a5a669bebe60b131e78
Sveta <svetasw_17@mail.ru>
2022-11-27 15:31:53 +1100

    DZ13

---
+++ DZ13/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.WriteLine("Enter an integer number");
+int number = Convert.ToInt32(Console.ReadLine());
+int d = 0, k = 0;
+if (number > 99)
+{ 
+    k = number;
+    while (k >= 1000) 
+    { 
+            k /= 10;
+        d = k % 10;
+    }    
+Console.WriteLine($"The third number from the left is {d}");
+}
+else
+{
+    Console.WriteLine($"The number has fewer than 3 digits, ssory.");
+}
