4f455266d78b61808fd85e4d07eeb3d8ddb1924e
Sveta <svetasw_17@mail.ru>
2022-11-27 15:28:08 +1100

    DZ10

---
+++ DZ10/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Enter a three-digit integer number");
+int number = Convert.ToInt32(Console.ReadLine());
+int second_digit = number/10%10;
+Console.WriteLine($"The second digit of the number is {second_digit}");
+
